4e106a043115f289bc5d3add180a4d5ea01e0fd7
Jon Gallant <jon@jongallant.com>
2017-04-17 21:29:32 -0700

    Add clear

--- radioSlicer/src/visual.ts
+++ radioSlicer/src/visual.ts
@@ -5,6 +5,7 @@ module powerbi.extensibility.visual {
         private selectionIds: any = {};
         private host: IVisualHost;
         private isEventUpdate: boolean = false;
+        private lastSelectedValue: string;
 
         constructor(options: VisualConstructorOptions) {
             this.target = options.element;
@@ -57,18 +58,30 @@ module powerbi.extensibility.visual {
                 radio.value = value;
                 radio.name = "values";
                 radio.onclick = function (ev) {
+                    
                     this.isEventUpdate = true; // This is checked in the update method. If true it won't re-render, this prevents and infinite loop
                     this.selectionManager.clear(); // Clean up previous filter before applying another one.
 
-                    // Find the selectionId and select it
-                    this.selectionManager.select(this.selectionIds[value]).then((ids: ISelectionId[]) => {
-                        /*ids.forEach(function (id) {
-                            console.log(id);
-                        });*/
-                    });
+                    if (this.lastSelectedValue !== ev.srcElement.value) { // only create a selection if the values are different
+
+                        // Find the selectionId and select it
+                        this.selectionManager.select(this.selectionIds[value]).then((ids: ISelectionId[]) => {
+                            /*ids.forEach(function (id) {
+                                console.log(id);
+                            });*/
+                        });
+
+                        this.lastSelectedValue = value;
+
+                    } else {
+                        ev.srcElement.checked = false; // clear the radio button selection because they have selected a radio button that is already selected
+                        this.lastSelectedValue = "";
+                    }
 
                     // This call applys the previously selected selectionId
                     this.selectionManager.applySelectionFilter();
+
+
                 }.bind(this);
 
                 let label = document.createElement("label");
